7dd8db29dbb48b1fbb0feb668e594a1d28800f96
1297695247 <1297695247@qq.com>
2019-05-02 16:14:33 +0800

    主要优化camera类的设置，附带改了一下ray类

--- Project2/Camera.h
+++ Project2/Camera.h
@@ -2,17 +2,25 @@
 #include "Vector.h"
 #include "Ray.h"
 
+#define PI 3.1415926
 class Camera
 {
 public:
-	Camera(Vector eyePos ,Vector canvasOrgin,Vector canvasVertical ,Vector canvasHorizontal) 
+	Camera(float vfov,float aspect_ratio) 
 	{
-		orgin = eyePos;
-		lower_left_corner = canvasOrgin;
-		horizontal = canvasHorizontal;
-		vertical = canvasVertical;
+		float distant = 1;
+
+		float theta = vfov * PI / 180;
+		float half_height = tanf(theta / 2) * distant;
+		float half_width = aspect_ratio * half_height;
+
+		orgin = Vector(0, 0, 0);
+		lower_left_corner = Vector(-half_width, -half_height, -distant);
+		horizontal = Vector(2 * half_width, 0, 0);
+		vertical = Vector(0, 2 * half_height, 0);
 	}
-	Ray getRay(float u, float v) { return Ray(orgin, lower_left_corner + horizontal * u + vertical * v); };
+	//第一个参数u是水平上的比例，第二个是竖直上的比例
+	Ray getRay(float u, float v) { return Ray(orgin, lower_left_corner + horizontal * u + vertical * v - orgin); };
 	Vector orgin;
 	Vector lower_left_corner;
 	Vector horizontal;
--- Project2/main.cpp
+++ Project2/main.cpp
@@ -3,12 +3,24 @@
 #include "Camera.h"
 
 using namespace std;
+bool hitSphere(const Vector& center, float radius, const Ray& r)
+{
+	Vector o_minus_c = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(r.direction(), o_minus_c);
+	float c = dot(o_minus_c, o_minus_c) - radius * radius;
+	float discriminant = b * b - 4 * a * c;
+	
+	return (discriminant > 0);
+}
 
 Vector color(Ray ray)
 {
+	if (hitSphere(Vector(0, 0, -1), 0.5, ray))
+		return Vector(1,0,0);
 	Vector unit_direction = unit_vector(ray.direction());
 	float t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * Vector(1.0, 1.0, 1.0) + t * Vector(0.0,0.0,0.0);
+	return (1.0 - t) * Vector(1.0, 1.0, 1.0) + t * Vector(0.5,0.7,1.0);
 }
 int main()
 {
@@ -25,7 +37,12 @@ int main()
 	outtfile << "P3\n" << nx << " " << ny << "\n255\n";
 	#pragma endregion
 	
-	Camera camera(Vector(0, 0, 0), Vector(-2, -1, -1), Vector(0, 2, 0), Vector(4, 0, 0));
+	Camera camera(Vector(-2, 2, 1), Vector(0, 0, -1), Vector(0, 1, 0), 2, 2.0f);
+	//Vector orgin = Vector(0,0,0);
+	//Vector lower_left_corner = Vector(-2,-1,-1);
+	//Vector horizontal = Vector(4,0,0);
+	//Vector vertical = Vector(0,2,0);
+
 	//from left to right
 	//from top to bottom
 	for (int y = 0; y < ny; y++)
@@ -35,6 +52,7 @@ int main()
 			float u = (float)x / (float)nx;
 			float v = (float)y / (float)ny;
 			Ray ray = camera.getRay(u, v);
+			//Ray ray(orgin, lower_left_corner + horizontal * u + vertical * v - orgin);
 			Vector col = color(ray);
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
@@ -42,4 +60,5 @@ int main()
 			outtfile << ir << " " << ig << " " << ib << endl;
 		}
 	}
+	return 0;
 }
