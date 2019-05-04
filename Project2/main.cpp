b9d056b464bfcfea0a760434ffe6476a7f249f2e
1297695247 <1297695247@qq.com>
2019-05-04 19:49:53 +0800

    添加normal

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -3,7 +3,8 @@
 #include "Camera.h"
 
 using namespace std;
-bool hitSphere(const Vector& center, float radius, const Ray& r)
+
+float hitSphere(const Vector& center, float radius, const Ray& r)
 {
 	Vector o_minus_c = r.origin() - center;
 	float a = dot(r.direction(), r.direction());
@@ -11,15 +12,23 @@ bool hitSphere(const Vector& center, float radius, const Ray& r)
 	float c = dot(o_minus_c, o_minus_c) - radius * radius;
 	float discriminant = b * b - 4 * a * c;
 	
-	return (discriminant > 0);
+	if (discriminant < 0)
+		return -1.0;
+	else
+		return  (-b - sqrt(discriminant)) / (2.0 * a);
 }
 
 Vector color(Ray ray)
 {
-	if (hitSphere(Vector(0, 0, -1), 0.5, ray))
-		return Vector(1,0,0);
+	float t = hitSphere(Vector(0, 0, -1), 0.5, ray);
+	if (t > 0)
+	{
+		Vector n =  unit_vector(ray.point_at_parameter(t) - Vector(0, 0, -1));
+		return 0.5 * (n + Vector(1, 1, 1));
+	}
+		
 	Vector unit_direction = unit_vector(ray.direction());
-	float t = 0.5 * (unit_direction.y() + 1.0);
+	t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - t) * Vector(1.0, 1.0, 1.0) + t * Vector(0.5,0.7,1.0);
 }
 int main()
@@ -38,11 +47,7 @@ int main()
 	#pragma endregion
 	
 	Camera camera(Vector(0, 0, 0), Vector(-2, -1, -1), Vector(0, 2, 0), Vector(4, 0, 0));
-	//Vector orgin = Vector(0,0,0);
-	//Vector lower_left_corner = Vector(-2,-1,-1);
-	//Vector horizontal = Vector(4,0,0);
-	//Vector vertical = Vector(0,2,0);
-
+	
 	//from left to right
 	//from top to bottom
 	for (int y = 0; y < ny; y++)
@@ -52,7 +57,6 @@ int main()
 			float u = (float)x / (float)nx;
 			float v = (float)y / (float)ny;
 			Ray ray = camera.getRay(u, v);
-			//Ray ray(orgin, lower_left_corner + horizontal * u + vertical * v - orgin);
 			Vector col = color(ray);
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
